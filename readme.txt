19b8875d49c2a49f807a55d075d25c0f493e12eb
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2024-05-17 00:43:29 -0300

    readme agregado

---
+++ readme.txt
@@ -0,0 +1,12 @@
+comandos para que funcione 
+
+
+cd Pinteres-react
+npm install react-dom
+npm run dev
+
+para que funcione el link de
+
+npm run build
+
+npm run preview
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect,useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Logo from './components/icons/Logo';
@@ -8,108 +8,98 @@ import Masonry from '@mui/lab/Masonry';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useBookStore } from './store/bookStore';
 import Login from './components/Login';
-
 import appFirebase from './firebase';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-
+import debounce from 'lodash.debounce';
 
 const auth = getAuth(appFirebase);
 
 const api = createApi({
-  // Don't forget to set your access token here!
-  // See https://unsplash.com/developers
-  //accessKey: "iZKs0Ysj76XWq8Fn1X0sHJ-6cFWuh4G4hPgyeVzvlB4"
   accessKey: import.meta.env.VITE_ACCESKEY
 });
 
 function App() {
-// logica de login 
-const [usuario,setUsuario] =useState(null);
+  // Logica de login 
+  const [usuario, setUsuario] = useState(null);
 
-onAuthStateChanged(auth,(usuarioFirebase)=>{
-  if(usuarioFirebase){
-
-setUsuario(usuarioFirebase);
-  }
-  else {
-    setUsuario(null);
-  }
-})
-
-  const [hasMore, setHasMore] = useState(true)
-  const [data, setData] = useState([])
-  let index= useRef(1);
-
-  console.log("key: ",import.meta.env.VITE_ACCESKEY);
-
-const val= useBookStore(state => state.value )
-  console.log('data: ',data);
-  console.log('val: ',val);
-  
   useEffect(() => {
-index.current=1;
-setHasMore(true);
+    const unsubscribe = onAuthStateChanged(auth, (usuarioFirebase) => {
+      if (usuarioFirebase) {
+        setUsuario(usuarioFirebase);
+      } else {
+        setUsuario(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
+  const [hasMore, setHasMore] = useState(true);
+  const [data, setData] = useState([]);
+  let index = useRef(1);
 
-    api.search
-      .getPhotos({ query: val,perPage: 20,page: index.current  })
-      .then(result => {
-        setData(result.response.results);
-      })
-      .catch(() => {
-        console.log("something went wrong!");
-      });
-  }, [val]);
+  const val = useBookStore(state => state.value);
+  
+  const fetchPhotos = useCallback(
+    debounce((query, page) => {
+      api.search
+        .getPhotos({ query, perPage: 20, page })
+        .then(result => {
+          if (page === 1) {
+            setData(result.response.results);
+          } else {
+            setData(prevData => [...prevData, ...result.response.results]);
+          }
+          if (result.response.results.length === 0) {
+            setHasMore(false);
+          }
+        })
+        .catch(() => {
+          console.log("Something went wrong!");
+        });
+    }, 300),
+    []
+  );
+
+  useEffect(() => {
+    index.current = 1;
+    setHasMore(true);
+    fetchPhotos(val, index.current);
+  }, [val, fetchPhotos]);
 
   const moreData = () => {
-   
-    index.current = index.current + 1;
-    
-  
+    index.current += 1;
     if (index.current === 3) {
       setHasMore(false); // Actualiza el estado directamente a false
-      
     }
-  
-    api.search
-      .getPhotos({ query: val, perPage: 20, page: index.current })
-      .then(result => {
-        setData(data.concat(result.response.results));
-      })
-      .catch(() => {
-        console.log("something went wrong!");
-      });
+    fetchPhotos(val, index.current);
   }
-  
-  
-  return (
 
+  return (
     <div className='container'>
-    { usuario ? (
-      <>
-        <Header correoUsuario={usuario.email} />
-        <InfiniteScroll
-          dataLength={data.length}
-          next={moreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-          style={{overflow: 'none'}}
-        >
-          <Masonry 
-            columns={{xs:2,sm:3,md:5}}
-            spacing={{xs:1,sm:2,md:3}} 
-            className='masonry'> 
-            {data.map(item=>(
-              <Card key={item.id} item={item}/>
-            ))}
-          </Masonry>
-        </InfiniteScroll>
-      </>
-    ) : <Login /> }
-  </div>
-  
-  )
+      {usuario ? (
+        <>
+          <Header correoUsuario={usuario.email} />
+          <InfiniteScroll
+            dataLength={data.length}
+            next={moreData}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            style={{ overflow: 'none' }}
+          >
+            <Masonry 
+              columns={{ xs: 2, sm: 3, md: 5 }}
+              spacing={{ xs: 1, sm: 2, md: 3 }} 
+              className='masonry'
+            > 
+              {data.map(item => (
+                <Card key={item.id} item={item} />
+              ))}
+            </Masonry>
+          </InfiniteScroll>
+        </>
+      ) : <Login />}
+    </div>
+  );
 }
 
-export default App
-
+export default App;
